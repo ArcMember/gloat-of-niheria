d830f47dd8796a81456d6bff62b6808c131a2b4d
ArcMember <members.of.arc@gmail.com>
2024-04-05 15:29:19 +0300

    Update run.sh

--- startup/run.sh
+++ startup/run.sh
@@ -5,5 +5,5 @@ screen -S "niheria" -X quit
 screen -S "niheria-bot" -X quit
 
 # Start the new screens
-screen -dmS niheria bash -c "cd /home/gloat-of-niheria/server && node entry.express.mjs"
-screen -dmS niheria-bot bash -c "cd /home/gloat-of-niheria/bot && ts-node niherbot.ts"
+screen -dmS niheria bash -c "cd /root/gloat-of-niheria/server && node entry.express.mjs"
+screen -dmS niheria-bot bash -c "cd /root/gloat-of-niheria/bot && ts-node niherbot.ts"
